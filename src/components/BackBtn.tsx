653c71036445a028f6f0014f87976e4888d2bc5e
Luan Henrique <luanhsantos14@gmail.com>
2024-04-23 21:04:57 -0300

    criando página de repositórios

---
+++ src/components/BackBtn.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
+const BackBtn = () => {
+    const navigate = useNavigate()
+
+  return (
+    <button onClick={() => navigate(-1)}>Voltar</button>
+  )
+}
+
+export default BackBtn
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,9 +3,7 @@ import classes from './Loader.module.css'
 
 const Loader = () => {
   return (
-    <>
       <FaSpinner className={classes.loader} />
-    </>
   )
 }
 
